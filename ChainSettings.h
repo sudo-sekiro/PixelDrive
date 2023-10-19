5887882a3ef7fde51fefb23ac08cb8e65a957ad1
Jon Friedman <jfriedman97@gmail.com>
2023-10-19 21:48:21 +0200

    chainSettings: Add delay parameters
    Add delay parameters to the ChainSettings struct
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- ChainSettings.h
+++ ChainSettings.h
@@ -3,6 +3,7 @@ struct ChainSettings
 {
     float preGain {0.f};
     float distortionTone {1.f}, distortionPreGain {50.f}, distortionPostGain {0.f}, distortionClarity {1000.f};
+    float delayTime {0.f}, delayWetLevel {0.f}, delayFeedback {0.f};
     float reverbIntensity {0.5f}, reverbWetMix {0.33f}, reverbRoomSize {0.5f}, reverbSpread {1.f};
     bool reverbShimmer {false};
     float noiseGate {0.f};
