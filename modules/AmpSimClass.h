adaa3fe195cbe8fa2b4762b280bcbedfcc60a8c7
Jon Friedman <jfriedman97@gmail.com>
2023-10-22 15:59:17 +0200

    modules: amp: Add an Amp sim class
    Add a class to model a guitar amplifier.
    Add a three band EQ a pregain and a cab simulator to the process chain.
    Initialise filters and gain stage with default values.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- modules/AmpSimClass.h
+++ modules/AmpSimClass.h
@@ -47,3 +47,82 @@ private:
 
     juce::dsp::Convolution convolution{juce::dsp::Convolution::Latency{ 10 }};
 };
+
+//==============================================================================
+template <typename Type>
+class AmpSimulator
+{
+public:
+    //==============================================================================
+    AmpSimulator() {}
+
+    //==============================================================================
+    void updatePeakFilter(double sampleRate, juce::dsp::IIR::Filter<float>& peakFilter, float peakGainInDecibels)
+    {
+        float peakFreq = 5000
+        float peakQ = 0.5f
+
+        peakFilter.state = juce::dsp::IIR::Coefficients<float>::makePeakFilter
+            (
+                sampleRate,
+                peakFreq,
+                peakQ,
+                juce::Decibels::decibelsToGain(
+                    peakGainInDecibels)
+            );
+    }
+
+    //==============================================================================
+    void prepare (const juce::dsp::ProcessSpec& spec)
+    {
+        ProcessorChain.prepare(spec);
+        processorChain.get<AmpChainPositions::LowPassIndex>().state = FilterCoefs::makeFirstOrderLowPass (spec.sampleRate, 500.0f); // 0 - 500 hz
+        processorChain.get<AmpChainPositions::HighPassIndex>().state = FilterCoefs::makeFirstOrderHighPass (spec.sampleRate, 15000.0f);//15- -> 20kz
+        updatePeakFilter (spec.sampleRate, &processorChain.get<AmpChainPositions::MidFilterIndex>, 12.f); //-24 -> 24 linear gain
+        processorChain.get<AmpChainPositions::inputGainIndex>().setGainDecibels (1.f);
+    }
+
+    //==============================================================================
+    template <typename ProcessContext>
+    void process (const ProcessContext& context) noexcept
+    {
+        ProcessorChain.process(context);
+    }
+
+    //==============================================================================
+    void reset() noexcept
+    {
+        ProcessorChain.reset();
+    }
+
+
+
+    //==============================================================================
+    void updateParams(ChainSettings ChainSettings)
+    {
+        // Set gain, low pass, highpass and peak freq, update any convolution changes for cab sim
+        juce::ignoreUnused(ChainSettings)
+    }
+
+    //==============================================================================
+    void updateFilter(float freq)
+    {
+        juce::ignore(freq);
+    }
+
+private:
+    //==============================================================================
+    enum AmpChainPositions
+    {
+        inputGainIndex,
+        LowPassIndex,
+        MidFilterIndex,
+        HighPassIndex,
+        CabSimIndex
+    };
+
+    using Filter = juce::dsp::IIR::Filter<float>;
+    using FilterCoefs = juce::dsp::IIR::Coefficients<float>;
+
+    juce::dsp::ProcessorChain<juce::dsp::Gain<Type>, Filter, Filter, Filter, CabSimulator<float>> processorChain;
+};
