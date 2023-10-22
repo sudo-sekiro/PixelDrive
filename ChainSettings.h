d4792e935fc0968f89c59b2ac5c4b4605b5651ca
Jon Friedman <jfriedman97@gmail.com>
2023-10-22 16:14:56 +0200

    chainSettings: Add amp parameters
    Add paramaters that will be used to control the amp component
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- ChainSettings.h
+++ ChainSettings.h
@@ -3,6 +3,7 @@ struct ChainSettings
 {
     float preGain {0.f};
     float distortionTone {1.f}, distortionPreGain {50.f}, distortionPostGain {0.f}, distortionClarity {1000.f};
+    float ampInputGain {1.f}, ampLowEnd {0.f}, ampMids {0.f}, ampHighEnd {20000.f};
     float delayTime {0.f}, delayWetLevel {0.f}, delayFeedback {0.f};
     float reverbIntensity {0.5f}, reverbWetMix {0.33f}, reverbRoomSize {0.5f}, reverbSpread {1.f};
     bool reverbShimmer {false};
