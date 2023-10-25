e82270029b564def1e3bf49c920654ec6edb3634
Jon Friedman <jfriedman97@gmail.com>
2023-10-26 00:33:45 +0200

    chainSettings: Add bypass parameters
    Add booleans to bypass the distortion, amp, reverb and delay stages
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- ChainSettings.h
+++ ChainSettings.h
@@ -3,10 +3,13 @@ struct ChainSettings
 {
     float preGain {0.f};
     float distortionTone {1.f}, distortionPreGain {50.f}, distortionPostGain {0.f}, distortionClarity {1000.f};
+    bool distortionBypass {false};
     float ampInputGain {1.f}, ampLowEnd {0.f}, ampMids {0.f}, ampHighEnd {20000.f};
+    bool ampBypass {false};
     float delayTime {0.f}, delayWetLevel {0.f}, delayFeedback {0.f};
+    bool delayBypass {false};
     float reverbIntensity {0.5f}, reverbWetMix {0.33f}, reverbRoomSize {0.5f}, reverbSpread {1.f};
-    bool reverbShimmer {false};
+    bool reverbShimmer {false}, reverbBypass {false};
     float noiseGate {0.f};
 };
 
