4b4fa80ffc48d64a4b0c66935d4818ed7abdfa0a
Jon Friedman <jfriedman97@gmail.com>
2024-02-10 18:47:59 +0200

    ui: Draw backgound image for toggle button
    Include toggle images as binary data.
    
    Draw backgound image for toggle button.
    Load image from binary data and paint it.
    Check whether the buttn is on or off and whether it is pushed down
    and set the relevant image.
    
    Signed-off-by: Jon Friedman <jfriedman97@gmail.com>

--- UserInterface/CustomToggle.cpp
+++ UserInterface/CustomToggle.cpp
@@ -1,40 +1,49 @@
 #include "UserInterface/CustomToggle.h"
 
+#define TOGGLE_LABEL_COLOUR juce::Colours::black
+
 void CustomLookAndFeel::drawToggleButton(juce::Graphics& g,
                                    juce::ToggleButton& toggleButton,
                                    bool shouldDrawButtonAsHighlighted,
                                    bool shouldDrawButtonAsDown) {
     using juce::Rectangle, juce::ignoreUnused, juce::Colour, juce::jmin;
     ignoreUnused(shouldDrawButtonAsHighlighted, shouldDrawButtonAsDown);
+    juce::Image toggleBackground;
 
     auto bounds = toggleButton.getLocalBounds();
     // g.drawRect(bounds);
 
     auto textBox = bounds.removeFromTop(getTextHeight());
     auto str = toggleButton.getButtonText();
-    g.setColour(Colour(0u, 172u, 1u));
+    g.setColour(TOGGLE_LABEL_COLOUR);
     g.drawFittedText(str, textBox.toNearestInt(), juce::Justification::centred, 1);
 
     auto size = jmin(bounds.getWidth(), bounds.getHeight());
     auto r = bounds.withSizeKeepingCentre(size, size).toFloat();
     // g.drawRect(r);
 
-    Rectangle<float> base = r.withSizeKeepingCentre(size / 1.5f, size / 4.5f).toFloat();
-    base.setY(r.getCentreY());
+    if (!toggleButton.getToggleState()) {
+        // Use on state toggle background
+        if (toggleButton.isDown()) {
+            toggleBackground = ImageFileFormat::loadFrom(BinaryData::toggle_on_down_png,
+                                                         BinaryData::toggle_on_down_pngSize);
+        } else {
+            toggleBackground = ImageFileFormat::loadFrom(BinaryData::toggle_button_on_png,
+                                                         BinaryData::toggle_button_on_pngSize);
+        }
+    } else {
+        // Use off state toggle background
+        if (toggleButton.isDown()) {
+            toggleBackground = ImageFileFormat::loadFrom(BinaryData::toggle_off_down_png,
+                                                         BinaryData::toggle_off_down_pngSize);
+        } else {
+            toggleBackground = ImageFileFormat::loadFrom(BinaryData::toggle_button_off_png,
+                                                         BinaryData::toggle_button_off_pngSize);
+        }
+    }
 
-    auto buttonHeight = toggleButton.getToggleState() ?  size / 5 : size / 10;
-    Rectangle<float> cylinder = r.withSizeKeepingCentre(size / 2.f, static_cast<float>(buttonHeight)).toFloat();
-    cylinder.setBottom(base.getCentreY());
-    Rectangle<float> cylinderTop = cylinder.withTrimmedBottom(cylinder.getHeight() / 2);
-
-    auto colour = toggleButton.getToggleState() ?  juce::Colours::dimgrey : Colour(0u, 172u, 1u);
-    g.setColour(colour);
-    g.fillEllipse(base);
-    g.setColour(juce::Colours::black);
-    g.drawEllipse(base, 2.f);
-
-    g.setColour(juce::Colours::silver);
-    g.fillRoundedRectangle(cylinder, size / 8.f);
-    g.setColour(juce::Colours::black);
-    g.drawEllipse(cylinderTop, 2.f);
+    g.drawImage(toggleBackground,  // Image to draw
+                r.toFloat(),  // Rectangle to draw within
+                juce::RectanglePlacement::fillDestination,  // placementWithinTarget - default stretchToFit
+                false);  // fillAlphaChannelWithCurrentBrush
 }
